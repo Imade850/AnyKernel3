3a96de0f49716360b6910ca62e8cf83a1641cf5e
Imade850 <163065294+Imade850@users.noreply.github.com>
2024-05-23 15:59:48 +0700

    Update anykernel.sh
    change name

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Amogus Kernel by Rama850 @ xda-developers
+kernel.string=Amogus Kernel initial by Imade850 
 do.devicecheck=1
 do.modules=0
 do.systemless=1
